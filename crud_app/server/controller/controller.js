8b4e5a8a73ad898bab1b340d5e7150177edfc845
REJOINT <brecab120@gmail.com>
2023-07-11 20:53:27 -0400

    Find and Update using Postman

--- crud_app/server/controller/controller.js
+++ crud_app/server/controller/controller.js
@@ -31,12 +31,34 @@ exports.create = (req, res) => {
 
 //retrieve and return all users/ retrieve and return a single user
 exports.find=(req,res)=>{
-
+    Userdb.find()
+    .then(user=> {
+        res.send(user)
+    })
+    .catch(err=> {
+        res.status(500).send({message:err.message || "Error occurred while retrieving user information"})
+    })
 }
 
 //Update a new identified user by user id
 exports.update=(req,res)=>{
+    if(!req.body){
+        return res.status(400)
+        .send({message:"Data to update cannot be empty"})
+    }
 
+    const id = req.params.id;
+    Userdb.findByIdAndUpdate(id,req.body,{useFindAndModify: false})
+        .then(data=> {
+            if(!data){
+                res.status(404).send({message: `Cannot Update user with ${id}. User not found!`})
+            }else{
+                res.send(data)
+            }
+        })
+        .catch(err=>{
+            res.status(500).send({message: "Error Update user information"})
+        })
 }
 
 //Delete a user with specified user id in the request
