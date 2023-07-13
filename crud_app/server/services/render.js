6d1ce88ed2c473f9e609a789ab672b8051c7ac4c
REJOINT <brecab120@gmail.com>
2023-07-12 23:45:06 -0400

    Using API

--- crud_app/server/services/render.js
+++ crud_app/server/services/render.js
@@ -1,6 +1,15 @@
+const axios = require('axios');
+
 
 exports.homeRoutes=(req, res) => {
-    res.render('index');
+    //make a get request to /api/users
+    axios.get('http://localhost:8080/api/users')
+    .then(function(response){
+        res.render('index', {users:response.data});
+    })
+    .catch(err=>{
+        res.send(err);
+    })
 }
 
 exports.add_user = (req, res) => {
