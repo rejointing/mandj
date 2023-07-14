7bc9fb84a616dfa7692450284ab2e9aea5abe5da
REJOINT <brecab120@gmail.com>
2023-07-13 21:03:45 -0400

    DELETE method functioning

--- crud_app/assets/js/index.js
+++ crud_app/assets/js/index.js
@@ -27,3 +27,25 @@ $("#update_user").submit(function(event){
     })
 
 })
+
+if(window.location.pathname=="/"){
+    $ondelete= $(".table tbody td a.delete");
+    $ondelete.click(function(){
+        var id = $(this).attr("data-id")
+
+        var request = {
+            "url":`http://localhost:8080/api/users/${id}`,
+            "method":"DELETE",
+        }
+
+        if(confirm("Do you really want to delete this record?")){
+            $.ajax(request).done(function(response){
+                alert("Data Deleted Successfully!");
+                location.reload();
+            })
+        
+            }
+        
+        })
+
+}
